70bb66a70f24900befca728efdca971e6ff31318
Moroni Motta <dareevil7@gmail.com>
2024-05-13 22:58:40 -0300

    w3

--- database/index.js
+++ database/index.js
@@ -31,7 +31,14 @@ module.exports = {
 }
 } else {
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
+    host: process.env.DATABASE_HOSTNAME,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_SCHEMA,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
   module.exports = pool
 }
